5a535095db7c5a13190ce685d103a3c7ec0465ea
Payal Bute <162861490+PayalBute6@users.noreply.github.com>
2024-03-21 19:51:19 +0530

    Update Assign2Prog6.php

--- Assign2Prog6.php
+++ Assign2Prog6.php
@@ -1,12 +1,6 @@
 <?php 
-  
-// Storing the elements of the webpage into an array 
-$source_code = file('https://www.google.co.in'); 
-  
-// 1. traversing through each element of the array 
-// 2.printing their subsequent HTML entities 
+$source_code = file('https://www.geeksforgeeks.org/'); 
 foreach ($source_code as $line_number => $last_line) { 
     echo nl2br(htmlspecialchars($last_line) . "\n"); 
-} 
-  
+}   
 ?> 
